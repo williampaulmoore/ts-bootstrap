483722799585d841743b87a3d19f3d78ec0b5c89
Paul Moore <williampaulmoore@gmail.com>
2023-07-22 11:38:42 +0100

    feat: make Tap test output format optional

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,24 @@
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    reporters: [
-        "jest-tap-reporter"
-    ],
+    reporters : getReporters(),
 };
+
+function getReporters() {
+    const result = ['default'];
+
+    if(process.env.QA_TESTRUN) {
+        result.push(tapReporter());
+    }
+
+    return result;
+
+    function tapReporter() {
+        return [
+            'jest-tap-reporter',
+            {
+                filePath: `${process.env.QA_TESTRUN}.tap`
+            }
+        ]
+    }
+}
